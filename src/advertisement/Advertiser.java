669a05621efaa0eea7e18726d6a9b56b44f9eb32
AlbinOlausson <albin.olausson@gmail.com>
2014-11-17 16:04:47 +0100

    changed user, advertiser and handleaccountbalance

--- src/advertisement/Advertiser.java
+++ src/advertisement/Advertiser.java
@@ -3,10 +3,53 @@ package advertisement;
 import general.User;
 
 /**
- * 
- * @author Axel Sigl
+ * Handles the advertiser
+ * @author Albin
  *
  */
-public class Advertiser extends User{
-	private AdvertiserTools defineAdvertisementScheme;
+public class Advertiser extends User {
+    private HandleAccountBalance balance;
+    
+    /**
+     * Sets the name and password for the user and adds credits to their balance
+     * @author Albin Olausson
+     * @param name
+     * @param id
+     * @param password
+     * @param credits
+     */
+    public Advertiser(String name, String password, int credits){
+        super(name, password);
+        balance = new HandleAccountBalance(credits);
+    }       
+    
+    /**
+     * Used to withdraw credits from the advertiser
+     * @author Albin Olausson
+     * @param amount
+     * @return
+     */
+    public boolean withdraw(int amount){
+        return balance.withdraw(amount);
+    }
+    
+    
+    /**
+     * Let's the advertiser add credits to their account
+     * @author Albin Olausson
+     * @param amount
+     * @return
+     */
+    public boolean deposit(int amount){
+        return balance.deposit(amount);
+    }
+    
+    /**
+     * Returns the advertisers balance
+     * @author Albin Olausson
+     * @return
+     */
+    public int getBalance(){
+        return balance.getCredits();
+    }
 }
--- src/general/User.java
+++ src/general/User.java
@@ -2,10 +2,27 @@ package general;
 
 /**
  * 
- * @author Axel Sigl
+ * @author Albin Olausson
  *
  */
 public abstract class User {
+	private String name;
+	private String password;
 	private GeneralTools login;
 	
+	
+
+	protected User(String name, String password){
+		this.name = name;
+		this.password = password;
+	}
+	
+	public String getName(){
+		return name;
+		
+	}
+	
+	public String getPassword(){
+		return password;
+	}
 }
