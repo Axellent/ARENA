e7c152a8c959956dc9a917ef5d503be002a69d8b
limeemil <andreas.lonnermark@gmail.com>
2014-11-25 20:08:17 +0100

    Added javadoc to my functions in CheckStatistics

--- src/general/CheckStatistics.java
+++ src/general/CheckStatistics.java
@@ -12,24 +12,46 @@ import play.Player;
 public class CheckStatistics extends GeneralTools{
 	private Player player;
 	
-	
+	/**
+	 * @author Andreas Lönnermark
+	 */
 	public CheckStatistics(Player player){
 		this.player=player;
 		
 	}
 	
+	/**
+	 * Returns the number of matches played
+	 * @author Andreas Lönnermark
+	 * @return
+	 */
 	public int getMatchesPlayed(){
 		return player.getMatchesPlayed();
 	}
 	
+	/**
+	 * Returns the number of matches won
+	 * @author Andreas Lönnermark
+	 * @return
+	 */
 	public int getMatchesWon(){
 		return player.getMatchesWon();
 	}
 	
+	/**
+	 * Returns the number of matches lost
+	 * @author Andreas Lönnermark
+	 * @return
+	 */
 	public int getMatchesLost(){
 		return player.getMatchesLost();
 	}
 	
+	/**
+	 * Returns the ranking
+	 * @author Andreas Lönnermark
+	 * @return
+	 */
 	public int getRanking(){
 		return player.getRanking();
 	}
