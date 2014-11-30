7cd93e3742f2f4f6600162f7794d71554996aee5
Axellent <axel.sigl@hotmail.com>
2014-11-30 03:04:01 +0100

    Fixed errounous merge

--- src/general/Arena.java
+++ src/general/Arena.java
@@ -17,9 +17,5 @@ public class Arena {
     	//connection.closeConnection();
     	
         new ArenaFrame();
-<<<<<<< HEAD
-=======
-        //new LeagueOwner("ownerName");
->>>>>>> 4bdffc18e80437cf63a27232c0e8d252f4d941ae
     }
 }
