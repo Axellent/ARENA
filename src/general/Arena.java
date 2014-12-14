8f83314159efe8e07568a83d5ac6b7817e5f5866
Axellent <axel.sigl@hotmail.com>
2014-12-14 15:50:45 +0100

    merged Arena class

--- src/general/Arena.java
+++ src/general/Arena.java
@@ -1,11 +1,8 @@
 package general;
 
-<<<<<<< HEAD
 import play.Game;
 import GUI.ArenaFrame;
-=======
 import GUI.Controller;
->>>>>>> 95d8fac490ebaeeb9385255284de8148f3eb226a
 import general.Connection;
 
 /**
@@ -17,22 +14,8 @@ public class Arena {
 	
 
     public static void main(String[]args){
-<<<<<<< HEAD
         new ArenaFrame();
-=======
-        
-    	
     	Connection connection = new Connection();
     	new Controller(connection);
-        //connection.command("help");
-        //System.out.println(connection.requestTournament("testTournament").getName());
-        //connection.command("login testName testPassword");
-        //connection.closeConnection();
-    	
-
-    	
-    	
-        
->>>>>>> 95d8fac490ebaeeb9385255284de8148f3eb226a
     }
 }
